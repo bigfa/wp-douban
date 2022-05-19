cd9def2a7de785777dac3627d6323bcc39d55e8f
bigfa <4398929@qq.com>
2022-05-19 18:49:23 +0800

    feat:add name and subtitle in edit template

--- tpl/tpl-subject-edit.php
+++ tpl/tpl-subject-edit.php
@@ -9,21 +9,28 @@
         <table class="form-table">
             <tbody>
                 <tr valign="top">
+                    <th scope="row"><label for="url">条目</label></th>
+                    <td>
+                        <p><?php echo $subject->name; ?></p>
+                        <p><?php echo $subject->card_subtitle; ?></p>
+                    </td>
+                </tr>
+                <tr valign="top">
                     <th scope="row"><label for="url">观看时间</label></th>
                     <td>
-                        <input type="datetime" name="create_time" value="<?php echo $fave->create_time ?>"></input>
+                        <input type="datetime" name="create_time" value="<?php echo $fave->create_time ?>" class="regular-text"></input>
                     </td>
                 </tr>
                 <tr valign="top">
                     <th scope="row"><label for="url">短评</label></th>
                     <td>
-                        <textarea name="remark" class="wp-editor-area"><?php echo $fave->remark ?></textarea>
+                        <textarea name="remark" style="width: 600px;" rows="5" cols="30" placeholder="输入短评"><?php echo $fave->remark ?></textarea>
                     </td>
                 </tr>
                 <tr valign="top">
                     <th scope="row"><label for="url">评分</label></th>
                     <td>
-                        <input name="score" type="number" value="<?php echo $fave->score ?>" min="0" max="5"></input>
+                        <input name="score" type="number" value="<?php echo $fave->score ? $fave->score : 0 ?>" min="0" max="5"></input>
                     </td>
                 </tr>
             </tbody>
@@ -33,7 +40,7 @@
         <input type="hidden" name="fave_id" value="<?php echo $fave->id ?>" />
         <input type="hidden" name="subject_type" value="<?php echo $fave->type ?>" />
         <div class="nm-submit-form">
-            <input type="submit" class="button-primary muhermit_submit_form_btn" name="save" value="<?php _e('Save Changes') ?>" />
+            <input type="submit" class="button-primary" name="save" value="<?php _e('Save Changes') ?>" />
         </div>
     </form>
 </div>
